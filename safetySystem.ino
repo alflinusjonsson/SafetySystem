73d068b027faedb233e09d1814dd44c48d4f9dc1
Linus Jönsson <32966642+alflinusjonsson@users.noreply.github.com>
2019-09-17 10:00:43 +0200

    Add files via upload

--- safetySystem.ino
+++ safetySystem.ino
@@ -1,3 +1,4 @@
+
 #include <LiquidCrystal.h>
 #include <SPI.h>
 #include <MFRC522.h>
@@ -21,70 +22,90 @@
 #define UNSAFE 0
 #define IN 1
 #define OUT 0
-#define DEADorALIVE 0
-#define REACTORROOM 1.6
+#define REACTOR_ROOM 1
+#define CONTROL_ROOM 2
+#define BREAK_ROOM 3
 
 MFRC522 mfrc522(SDA, RST);
 LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
-const int buttonPin = 8;
-const int buzzerPin = A1;
 
-int radiationInput = A0;
+const int clothButton = 8;
+const int roomButton = A2;
+const int buzzerPin = A1;
+const int radiationInput = A0;
+
 int radiationLevel = 0;
 int radiationLevelMin = 0;
 int radiationLevelMax = 1023;
 int previousRad = 0;
-
-int reading = 0;
+int clothButtonV = 0;
+int roomButtonV = 0;
 
 int stamp = OUT;
+long E = 0;
+float rc = 1.6; // reactor room default
+char pcV = 'C'; // normal cloth default
+char room = 'R';
 
-long RPS_UNIT = 0;
-char PPE = 'C';
-int buttonState = 0; 
-
-int state = SAFE;      // the current state of the output pin
+int pc = SAFE;      // default state protective cloth
+int state = REACTOR_ROOM; // default room
 
 void setup() {
   // set up the LCD's number of columns and rows:
   lcd.begin(16,2);
-
-  lcd.setCursor(15,0);
-  lcd.print(PPE);
  
   // set up rfid
   SPI.begin();
   mfrc522.PCD_Init();
-
+  
   Serial.begin(9600);
 
   // set buzzer as output and turn it off
   pinMode(buzzerPin, OUTPUT);
   analogWrite(buzzerPin, 255);
+
+  // set roomPin button as input
+  pinMode(roomButton, INPUT);
  
-  // set button as an input
-  pinMode(buttonPin, INPUT);
+  // set clothPin button as an input
+  pinMode(clothButton, INPUT);
 
   // set timer to 0 as default
   setTime(0,0,0,0,0,0000);
+
+  lcd.print("Please clock in");
 }
 
-void timers(){
- 
-  lcd.setCursor(0,2);
-  lcd.print(minute());
-  lcd.print(":");
-  lcd.print(second());
-  lcd.print(" ");
-  
-  lcd.setCursor(1,2);
-  lcd.print("Max: ");
-  lcd.print(calculateRPS());
-
-  if (calculateRPS() >= minute())
+void state_machine_run() 
+{
+  switch(state)
   {
-    lcd.clear();
-    lcd.print("WARNING! CHECK OUT!");
+   case REACTOR_ROOM:
+      rc = 1.6;
+      state = CONTROL_ROOM;
+      room = 'R';
+      lcd.setCursor(7,2);
+      lcd.print("ROOM:");
+      lcd.print(room);
+   break;
+
+   case CONTROL_ROOM:
+      rc = 0.5;
+      state = BREAK_ROOM;
+      room = 'C';
+      lcd.setCursor(7,2);
+      lcd.print("ROOM:");
+      lcd.print(room);
+   break;
+
+   case BREAK_ROOM:
+      rc = 0.1;
+      state = REACTOR_ROOM;
+      room = 'B';
+      lcd.setCursor(7,2);
+      lcd.print("ROOM:");
+      lcd.print(room);
+   break;
   }
 }
 
@@ -97,68 +118,57 @@ void sendBluetooth(){
   Serial.print(",");
   Serial.print(second());
   Serial.print(",");
-  Serial.print(PPE);
+  Serial.print(pcV);
   Serial.print(",");
   Serial.print(radiationLevel);
   Serial.print(",");
-  Serial.print(DEADorALIVE);
-}
+  Serial.print(room);
 
-int calculateRPS(){
-  
-  if (PPE == 'C'){
-    RPS_UNIT = (REACTORROOM * radiationLevel)/1;
-  }
-
-  if (PPE == 'S'){
-    RPS_UNIT = (REACTORROOM * radiationLevel)/5;
-  }
-
-  long maxAllowedDailyRad = 500000/RPS_UNIT;
-  
-  return maxAllowedDailyRad/60;
+  delay(200);
 }
 
 void loop() {
 
-  // send information over bluetooth
-  sendBluetooth();
-
   // read the state of radiation
   radiationLevel = analogRead(radiationInput);
   radiationLevel = map(radiationLevel, radiationLevelMin, radiationLevelMax, 1, 100);
-  lcd.setCursor(14,2);
-  if (radiationLevel != previousRad){
-    lcd.print(radiationLevel);
+  if (stamp == IN){
+    if (radiationLevel != previousRad){
+        lcd.setCursor(10,0);
+        lcd.print(radiationLevel);
+        lcd.print("   ");
+    }
+
+    sendBluetooth();
   }
   previousRad = radiationLevel;
  
-  reading = digitalRead(buttonPin);
+  clothButtonV = digitalRead(clothButton);
+  roomButtonV = analogRead(roomButton);
   
-  if (reading == HIGH) {
+  if (roomButtonV > 1000){
+    delay(50);
+    if (roomButtonV > 1000){
+      state_machine_run();
+    }
+  }
+  
+  if (clothButtonV == HIGH) {
     delay(20);
-    if (state == SAFE && reading == HIGH) {
-      state = UNSAFE;
-      lcd.setCursor(15,0);
+    if (pc == SAFE && clothButtonV == HIGH) {
+      pc = UNSAFE;
+      lcd.setCursor(5,2);
       lcd.print("S");
-      PPE = 'S';
-      lcd.setCursor(14,2);
-      lcd.print(radiationLevel);
+      pcV = 'S';
       }
-    else if (state == UNSAFE && reading == HIGH) {
-      state = SAFE;
-      lcd.setCursor(15,0);
+    else if (pc == UNSAFE && clothButtonV == HIGH) {
+      pc = SAFE;
+      lcd.setCursor(5,2);
       lcd.print("C");
-      PPE = 'C';
-      lcd.setCursor(14,2);
-      lcd.print(radiationLevel);
+      pcV = 'C';
     }   
   }
-
-  if (stamp == IN){
-    timers();
-  }
-
+  
   //look for card
   if ( !mfrc522.PICC_IsNewCardPresent())
   {
@@ -185,7 +195,8 @@ void loop() {
   }
   
   Serial.println();
-  Serial.print("Message : ");
+  Serial.print("Message : Tech1");
+  
   content.toUpperCase();
 
   if (content.substring(1) == "96 FE 0A F8" && stamp == OUT)
@@ -193,9 +204,24 @@ void loop() {
     analogWrite(buzzerPin, 0);
     delay(50);
     analogWrite(buzzerPin, 255);
+    lcd.clear();
     lcd.home();
     lcd.print("IN ");
-    stamp = IN; 
+    stamp = IN;
+    Serial.print("IN");
+    delay(2000);
+    lcd.home();
+    lcd.print("Select gear&room");
+    delay(2000);
+    lcd.clear();
+    lcd.print("Radiation:");
+    lcd.setCursor(0,2);
+    lcd.print("GEAR:");
+    lcd.setCursor(5,2);
+    lcd.print(pcV);
+    lcd.setCursor(7,2);
+    lcd.print("ROOM:");
+    lcd.print(room);
     delay(1000); 
     
     setTime(0, 0, 0, 0, 0, 0000); 
@@ -206,16 +232,15 @@ void loop() {
     analogWrite(buzzerPin, 0);
     delay(50);
     analogWrite(buzzerPin, 255);
+    lcd.clear();
     lcd.home();
     lcd.print("OUT"); 
     stamp = OUT;
-    delay(1000);
-    
+    Serial.print("OUT");
+    delay(2000);
+    lcd.clear();
+    lcd.print("Please clock in");
     setTime(0, 0, 0, 0, 0, 0000);
-    lcd.setCursor(0,2);
-    lcd.print("0");
-    lcd.print(":");
-    lcd.print("00");
    
   }
 }
